bd152d9bf5a15be87e26ef42870f28b8f75f8dc1
francescotroiano98 <francesco-troiano-98@libero.it>
2023-07-14 16:32:38 +0200

    added 2 classes CatProduct and DogProduct

--- db/db.php
+++ db/db.php
@@ -1,12 +1,13 @@
 <?php 
 
     include_once __DIR__ . '/../classes/Category.php';
-    include_once __DIR__ . '/../classes/product.php';
+    include_once __DIR__ . '/../classes/DogProduct.php';
+    include_once __DIR__ . '/../classes/CatProduct.php';
 
 
     $categories = [
         new Category ('Dogs');
-        new Category ('Cats')
+        new Category ('Cats');
     ]; //qui andrà messo: cani e gatti
 
     $dogsProducts = []; //qui ci andrà messo: Cibo,Gioco,Cuccia,Pulizia,Accessori
