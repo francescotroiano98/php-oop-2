d9a40666401de01188526a3baad6d75aaf266d81
francescotroiano98 <francesco-troiano-98@libero.it>
2023-07-14 17:51:24 +0200

    added card

--- index.php
+++ index.php
@@ -15,6 +15,31 @@
 
 </head>
 <body>
+
+    <header>
+        <h1>DOGS $ CATS SHOP</h1>
+    </header>
+    <main>
+        <section class="container">
+
+                <div class="row row-cols-1 row-cols-md-3 g-4">
+                <?php foreach ($categories as $category) { ?>
+                    <?php foreach ($category->products as $subcategory => $product) { ?>
+                    <div class="col">
+                        <div class="card">
+                            <img src="<?php echo $product->image; ?>" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title"><?php echo $product->title; ?></h5>
+                            <p class="card-text">Price: $<?php echo $product->price; ?></p>
+                        </div>
+                        </div>
+                    </div>
+                    <?php } ?>
+                <?php } ?>
+            </div>        
+
+        </section>
+    </main>
     
 </body>
 </html>
