59a42c9c3c4c9cab1b10428d98ddc86754cf9293
francescotroiano98 <francesco-troiano-98@libero.it>
2023-07-14 18:41:11 +0200

    added layout

--- classes/Category.php
+++ classes/Category.php
@@ -3,15 +3,20 @@
     class Category {
 
         public $name;
+        public $icon;
         public $products;
 
-        public function __construct($name, $products = []) {
+        public function __construct($name, $icon, $products = []) {
             $this->name = $name;
+            $this->icon = $icon;
             $this->products = $products;
         }
 
         public function getName(){
             return $this->name;
+        }
+        public function getIconPath(){
+            return $this->icon;
         }
 
         public function addProduct($product) {
--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 
-    <link rel="stylesheet" href="./styles/style.css">
+    <link rel="stylesheet" href="styles/style.css">
 
 
 </head>
@@ -20,24 +20,69 @@
         <h1>DOGS $ CATS SHOP</h1>
     </header>
     <main>
-        <section class="container">
-
-                <div class="row row-cols-1 row-cols-md-3 g-4">
+    <section class="container">
+            <h2>Dogs</h2>
+            <div class="row">
                 <?php foreach ($categories as $category) { ?>
-                    <?php foreach ($category->products as $subcategory => $product) { ?>
-                    <div class="col">
-                        <div class="card">
-                            <img src="<?php echo $product->image; ?>" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title"><?php echo $product->title; ?></h5>
-                            <p class="card-text">Price: $<?php echo $product->price; ?></p>
-                        </div>
-                        </div>
-                    </div>
+                    
+                    <?php if ($category->name === 'Dogs') { ?>
+                        <?php foreach ($category->products as $subcategory => $product) { ?>
+                            <div class="col-3">
+                                <div class="card">
+                                    <div class="icon-size d-flex align-items-center">
+                                    <h3 class="me-2">
+
+                                        <?php echo $category->name; ?>
+                                    </h3>    
+                                        <img src="<?php echo $category->icon; ?>" alt="<?php echo $category->name; ?> Icon">
+                                
+                                    </div>
+                                
+                                    
+                                    <div class="card-body">
+                                        <div class="image-container">
+                                            <img src="<?php echo $product->image; ?>" class="card-img-top" alt="<?php echo $product->title; ?>">
+                                        </div>
+                                    
+                                        <h5 class="card-title"><?php echo $product->title; ?></h5>
+                                        <p class="card-text">Price: $<?php echo $product->price; ?></p>
+                                    </div>
+                                </div>
+                            </div>
+                        <?php } ?>
                     <?php } ?>
                 <?php } ?>
-            </div>        
+            </div>
 
+            
+            <div class="row">
+                <?php foreach ($categories as $category) { ?>
+                    
+                    <?php if ($category->name === 'Cats') { ?>
+                        <?php foreach ($category->products as $subcategory => $product) { ?>
+                            <div class="col-3">
+                                <div class="card">
+                                    <div class="icon-size d-flex align-items-center">
+                                        <h3 class="me-2">
+                                            <?php echo $category->name; ?>
+                                        </h3>
+                                        <img src="<?php echo $category->icon; ?>" alt="<?php echo $category->name; ?> Icon">
+                                    </div>
+                                    
+                                    
+                                    <div class="card-body">
+                                        <div class="image-container">
+                                            <img src="<?php echo $product->image; ?>" class="card-img-top" alt="<?php echo $product->title; ?>">
+                                        </div>    
+                                        <h5 class="card-title"><?php echo $product->title; ?></h5>
+                                        <p class="card-text">Price: $<?php echo $product->price; ?></p>
+                                    </div>
+                                </div>
+                            </div>
+                        <?php } ?>
+                    <?php } ?>
+                <?php } ?>
+            </div>
         </section>
     </main>
     
